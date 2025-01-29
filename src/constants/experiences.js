ee420c10e4d03ebbac0e8f201ae38e269bd25197
higueromora <higueromora@hotmail.com>
2025-01-29 17:17:54 +0100

    update logos

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -5,7 +5,7 @@ export const experiences = [
       period: "April - December 2024 | 9 months",
       description: `Development of an app for real estate management using HTML, CSS, jQuery, PHP, SQL and 
                     Updating legacy code with jQuery and PHP. `,
-      description2: `Development of a website and creation of a multivendor platform and hooks for WordPress, along with analysis of SQL databases with market weight. 
+      description2: `Development of a website and creation of a multivendor platform and hooks for WordPress, along with analysis of SQL databases linked to a commercial weighing scale. 
       The goal is to connect WordPress products and the balance through SQL queries.`, 
       description3: `Optimization of performance and responsive interface design to enhance user experience on both mobile and desktop devices .`,          
     }
