34a5244348f6d5a58417fd8727be98e3c132499b
higueromora <higueromora@hotmail.com>
2025-02-08 02:10:11 +0100

    size img

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -4,8 +4,8 @@ export const CardProfile = ({ pictureCV, pictureCV2 }) => {
             <h1 className="name">Ángel HM</h1>
             <div style={{ width: '100%', height: '253px' }}>
                 <picture className="picture-container">
-                    <source srcSet={pictureCV} alt="profile" style={{ width: '100%', borderRadius: '50%' }} type="image/webp" />
-                    <img loading='eager' src={pictureCV2} alt="profile" style={{ width: '100%', borderRadius: '50%' }} />
+                    <source srcSet={pictureCV} alt="profile" style={{ width: '254px', borderRadius: '50%' }} type="image/webp" />
+                    <img loading='eager' src={pictureCV2} alt="profile" style={{ width: '254px', borderRadius: '50%' }} />
                 </picture>
             </div>
             <h2 className="category">Web developer</h2>
