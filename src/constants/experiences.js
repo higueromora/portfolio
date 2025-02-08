41205a569a2bff39ede55a5b5f0b8c195e0311e6
higueromora <higueromora@hotmail.com>
2025-02-08 18:37:21 +0100

    upd experienciaen 3 persona

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -3,9 +3,9 @@ export const experiences = [
       role: "Full Stack Web Developer",
       company: "TechnoTouch",
       period: "Abril - Diciembre 2024 | 9 meses",
-      description: `Desarrollé una app de gestión inmobiliaria con tiendas, anuncios y asignación de vendedores. Diseñé la interfaz, la base de datos e implementé un sistema de QR para acceder a los anuncios.
+      description: `Desarrollo de una app de gestión inmobiliaria con tiendas, anuncios y asignación de vendedores. Diseño de interfaz, base de datos, usuarios e implementación de un sistema de QR para acceder a los anuncios.
       `,
       description2: `
-      Creé una plataforma multivendor en WordPress para un mercado con carnicerías y pescaderías. Sincronicé los productos de las básculas con el multivendor para que se actualicen automáticamente.`,          
+      Creación de una plataforma multivendor en WordPress para un mercado con varias tiendas. Se sincronizaron los productos de las básculas con el multivendor para que se actualicen automáticamente.`,          
     }
   ];
