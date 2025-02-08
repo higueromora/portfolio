fbd2e868b0c26ad1c9bc73c40d34f7ab77ca343b
higueromora <higueromora@hotmail.com>
2025-02-08 01:25:38 +0100

    portfolioprovisional2

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -3,10 +3,10 @@ export const experiences = [
       role: "Full Stack Web Developer",
       company: "TechnoTouch",
       period: "Abril - Diciembre 2024 | 9 meses",
-      description: `Desarrollo de una app de gestión inmobiliaria con tiendas, anuncios y asignación de vendedores. Diseñé la interfaz, la base de datos e implementé un sistema de QR para acceder a los anuncios.
+      description: `Desarrollé una app de gestión inmobiliaria con tiendas, anuncios y asignación de vendedores. Diseñé la interfaz, la base de datos e implementé un sistema de QR para acceder a los anuncios.
       `,
       description2: `
-      Creación de una plataforma multivendor en WordPress para un mercado con carnicerías y pescaderías. Con el objetivo de sincronizar automáticamente los productos de las básculas con el multivendor a través de la base de datos.`, 
-      description3: `Desarrollo de una página web cumpliendo las reglas accesibilidad WACC para dar a conocer productos y servicios`,          
+      Creé una plataforma multivendor en WordPress para un mercado con carnicerías y pescaderías. Con el objetivo de sincronizar automáticamente los productos de las básculas con el multivendor a través de la base de datos.`, 
+      description3: `Desarrollé una página web cumpliendo las reglas accesibilidad WACC para dar a conocer productos y servicios`,          
     }
   ];
