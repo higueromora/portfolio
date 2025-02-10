2525ee2427de9cc4be51296f70b354e90d4bce7b
higueromora <higueromora@hotmail.com>
2025-02-10 20:25:09 +0100

    s

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -2,7 +2,7 @@ export const CardProfile = ({ pictureCV, pictureCV2 }) => {
     return (
         <div className="card">
             <h1 className="name">Ángel HM</h1>
-            <div style={{ width: '100%', height: '253px' }}>
+            <div style={{ width: '100%', height: '253px', display: 'flex', justifyContent: 'center' }}>
                 <picture className="picture-container">
                     <source srcSet={pictureCV} alt="profile" style={{ width: '254px', borderRadius: '50%' }} type="image/webp" />
                     <img loading='eager' src={pictureCV2} alt="profile" style={{ width: '254px', borderRadius: '50%' }} />
