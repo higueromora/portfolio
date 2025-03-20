ae0677b11467d4605bed1343b8406d20371642d7
higueromora <higueromora@hotmail.com>
2025-03-20 23:27:42 +0100

    add experience

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -6,6 +6,6 @@ export const experiences = [
       description: `Desarrollo de una app de gestión inmobiliaria con tiendas, anuncios y asignación de vendedores. Diseño de interfaz, base de datos, usuarios e implementación de un sistema de QR para acceder a los anuncios.
       `,
       description2: `
-      Creación de una plataforma multivendor en WordPress para un mercado con varias tiendas. Se sincronizaron los productos de las básculas con el multivendor para que se actualicen automáticamente.`,          
+      Creación de una plataforma multivendor en WordPress para un mercado con varias tiendas. Se sincronizaron los productos de las básculas con el multivendor para que se actualicen automáticamente. Utilizando hooks, bases de datos y un sistema que hace de intermediario en la comunicación de datos.`,          
     }
   ];
